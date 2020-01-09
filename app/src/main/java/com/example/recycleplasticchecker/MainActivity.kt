7d3196093cae125ed26a1bbc74be6751240e6247
brianooi123 <ooikelvin3030@gmail.com>
2020-01-10 06:20:52 +0800

    user point done

--- app/src/main/java/com/example/recycleplasticchecker/MainActivity.kt
+++ app/src/main/java/com/example/recycleplasticchecker/MainActivity.kt
@@ -15,9 +15,12 @@ import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.popup1.*
 
@@ -26,13 +29,15 @@ private const val TAG : String = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    class User(val name: String = "", val email: String = "", val username: String = "", val password: String = "", val point: Int)
+    class User(val name: String, val email: String, val username: String, val password: String, val point: Int)
 
     lateinit var myDialog: Dialog
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var mAuth : FirebaseAuth
-
-
+    lateinit var databaseReference : DatabaseReference
+    lateinit var user : FirebaseUser
+    lateinit var uid : String
+    lateinit var point:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,27 +63,30 @@ class MainActivity : AppCompatActivity() {
     lateinit var redeemShopee:TextView
     lateinit var  redeemZalora:TextView
     lateinit var  redeemEbay:TextView
-    fun ShowPopup1(v:View){
+
+    fun ShowPopup1(v:View) {
 
         myDialog.setContentView(R.layout.popup1)
         tv_close = myDialog.findViewById(R.id.tv_cancel)
         btnRedeem = myDialog.findViewById(R.id.btnRedeem)
-        redeemLazada=myDialog.findViewById(R.id.lazadaCode)
-        redeemLazada.isGone=true
-        //redeemLazada.visibility=gone
-        tv_close.setOnClickListener(object: View.OnClickListener{
+        redeemLazada = myDialog.findViewById(R.id.lazadaCode)
+        redeemLazada.isGone = true
+
+        tv_close.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 myDialog.dismiss()
 
             }
         })
         btnRedeem.setOnClickListener {
+            redeem()
             redeemLazada.isGone=false
         }
         myDialog.show()
+
     }
 
-    fun ShowPopup2(v:View){
+        fun ShowPopup2(v:View){
 
         myDialog.setContentView(R.layout.popup2)
         tv_close = myDialog.findViewById(R.id.tv_cancel)
@@ -91,9 +99,10 @@ class MainActivity : AppCompatActivity() {
             }
         })
         btnRedeem.setOnClickListener {
-            redeemShopee.isGone=false
+            redeem()
+            redeemShopee.isGone = false
         }
-        myDialog.show()
+            myDialog.show()
 
     }
 
@@ -110,6 +119,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
         btnRedeem.setOnClickListener {
+            redeem()
             redeemZalora.isGone=false
         }
         myDialog.show()
@@ -128,9 +138,40 @@ class MainActivity : AppCompatActivity() {
             }
         })
         btnRedeem.setOnClickListener {
+            redeem()
             redeemEbay.isGone=false
         }
         myDialog.show()
+    }
+
+     fun redeem(){
+        user = FirebaseAuth.getInstance().currentUser!!
+        uid = user.uid
+        val database = FirebaseDatabase.getInstance().getReference("Users").child(uid)
+
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var user_name = dataSnapshot.child("name").value.toString()
+                var user_email = dataSnapshot.child("email").value.toString()
+                var user_username = dataSnapshot.child("username").value.toString()
+                var user_password = dataSnapshot.child("password").value.toString()
+                var user_point = dataSnapshot.child("point").value.toString()
+
+                if(user_point >= 1000.toString()){
+                    val currentPoint=  user_point.toInt() - 1000
+                    val user = User(user_name, user_email, user_username, user_password, currentPoint)
+                    database.setValue(user)
+                }
+                else{
+                    Toast.makeText(this@MainActivity,"Your point is insufficient!!",Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
